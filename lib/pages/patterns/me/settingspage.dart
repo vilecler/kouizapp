8063946629fd553507aeeca905d158441dac725f
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-05-26 22:14:03 +0200

    translating parameters

--- lib/pages/patterns/me/settingspage.dart
+++ lib/pages/patterns/me/settingspage.dart
@@ -5,7 +5,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:kouizapp/constants/customcolors.dart';
 import 'package:kouizapp/services/auth_cognito.dart' as auth;
 import 'package:kouizapp/utils/translation.dart';
-import 'package:kouizapp/widgets/titles/mediumtitlewidget.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 import '../../../widgets/hearders/backheaderwidget.dart';
@@ -38,60 +37,60 @@ class _SettingsPageState extends State<SettingsPage> {
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            BackHeaderWidget(title: "Settings", bolt: 100),
+            BackHeaderWidget(title: AppLocalizations.of(context)!.settings, bolt: 100),
 
             Flexible(
               child: SettingsList(
                 lightTheme: themeData,
                 sections: [
                   SettingsSection(
-                    title: Text('Application', style: sectionTextStyle),
+                    title: Text(AppLocalizations.of(context)!.general, style: sectionTextStyle),
                     tiles: [
                       SettingsTile(
-                        title: Text('Language'),
+                        title: Text(AppLocalizations.of(context)!.language),
                         description: Text(getLocaleValue(getSelectedLocale())),
-                        leading: FaIcon(FontAwesomeIcons.language),
+                        leading: const FaIcon(FontAwesomeIcons.language),
                         onPressed: (BuildContext context) {},
                       ),
                       SettingsTile.switchTile(
-                          leading: FaIcon(FontAwesomeIcons.bell),
+                          leading: const FaIcon(FontAwesomeIcons.solidBell),
                           initialValue: true,
                           onToggle: (value) { },
-                          title: Text('Allow notifications')
+                          title: Text(AppLocalizations.of(context)!.allowNotifications)
                       )
                     ],
                   ),
 
                   SettingsSection(
-                    title: Text('User', style: sectionTextStyle),
+                    title: Text(AppLocalizations.of(context)!.user, style: sectionTextStyle),
                     tiles: [
                       SettingsTile(
-                        title: Text('Username'),
+                        title: Text(AppLocalizations.of(context)!.username),
                         description: Text('petitstring'),
-                        leading: FaIcon(FontAwesomeIcons.user),
+                        leading: const FaIcon(FontAwesomeIcons.userAlt),
                         onPressed: (BuildContext context) {},
                       ),
                       SettingsTile(
-                        title: Text('Pseudo'),
+                        title: Text(AppLocalizations.of(context)!.pseudo),
                         description: Text('@petitstring'),
-                        leading: FaIcon(FontAwesomeIcons.user),
+                        leading: const FaIcon(FontAwesomeIcons.userAlt),
                         onPressed: (BuildContext context) {},
                       ),
                       SettingsTile(
-                        title: Text('Email'),
+                        title: Text(AppLocalizations.of(context)!.email),
                         description: Text('vivien.leclercq@gmail.com'),
-                        leading: FaIcon(FontAwesomeIcons.user),
+                        leading: const FaIcon(FontAwesomeIcons.solidEnvelope),
                         onPressed: (BuildContext context) {},
                       ),
                       SettingsTile(
-                        title: Text('Password'),
+                        title: Text(AppLocalizations.of(context)!.password),
                         description: Text('***********'),
-                        leading: FaIcon(FontAwesomeIcons.user),
+                        leading: const FaIcon(FontAwesomeIcons.lock),
                         onPressed: (BuildContext context) {},
                       ),
                       SettingsTile(
-                        title: Text('Disconnect'),
-                        leading: FaIcon(FontAwesomeIcons.user),
+                        title: Text(AppLocalizations.of(context)!.disconnect),
+                        leading: const FaIcon(FontAwesomeIcons.userAltSlash),
                         onPressed: (BuildContext context) async {
                           await auth.disconnect();
                           Navigator.pushReplacementNamed(context, '/login');
