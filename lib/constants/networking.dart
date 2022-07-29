3381fe429b2a85cbdc5ffc175d7d8d438ed745fe
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-07-29 13:01:30 +0200

    Fixing network dependancies

--- lib/constants/networking.dart
+++ lib/constants/networking.dart
@@ -1,6 +1,6 @@
 class Networking{
-  static const String apiDomainName = 'z6svykjvz5o3vjsud5ry7pwoq40bdioz.lambda-url.eu-west-3.on.aws';
+  static const String apiDomainName = '6elgtjowyagcuhn2nub6imnjbi0hvngm.lambda-url.eu-west-3.on.aws';
 
-  static const String userPoolId = 'eu-west-3_ZMm3o6tSs';
-  static const String clientId = '1id909n9tf0p6k33o4v8tku1lp';
+  static const String userPoolId = 'eu-west-3_9i0ViDUoh';
+  static const String clientId = '5rkiuruffnbih61ktcqq2lmtob';
 }
